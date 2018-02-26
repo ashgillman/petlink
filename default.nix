fab08037d1719ca77251a0ad0c94bcb4e291476e
Ashley Gillman <ashley.gillman@csiro.au>
2018-02-26 14:16:30 +1000

    FIX: Remove temp files from local src in nix

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <mypkgs> {}
+{ pkgs ? import <nixpkgs> {}
 , stdenv ? pkgs.stdenv
 , pythonPackages ? pkgs.python3Packages
 }:
@@ -25,7 +25,15 @@ in
   pythonPackages.buildPythonPackage rec {
     pname = "petlink";
     version = "0.0.0";
-    src = ./.;
+    src = builtins.filterSource
+      (path: type:
+           ! (type == "directory" && baseNameOf path == "data")
+        && ! (type == "directory" && baseNameOf path == "build")
+        && ! (type == "directory" && baseNameOf path == ".eggs")
+        && ! (type == "directory" && baseNameOf path == ".gcroots")
+        && ! (type == "symlink"   && baseNameOf path == "result")
+      )
+      ./.;
 
     buildInputs = python_build_dependencies;
     propagatedBuildInputs = python_dependencies;
